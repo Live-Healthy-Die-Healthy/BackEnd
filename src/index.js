f4c7aa08215b229da81e1c0fe625ef0f99213b2b
Jaeboong <cbkjh0225@gmail.com>
2024-07-15 23:17:24 +0900

    test

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ const env = process.env.NODE_ENV || 'test';
 const config = require('../config/config.json')[env];
 const db = {};
 
-
-
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
