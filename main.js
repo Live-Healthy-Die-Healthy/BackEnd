7c5fb2bf7d8938a3f5c961b34a04bc8ca9657659
seungyun-Park <tmdrbs0925@gmail.com>
2024-07-22 11:47:50 +0900

    route분리

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const { sequelize, ExerciseLog, User, ExerciseList, AerobicExercise, AnaerobicExercise, DietLog, DietLogDetail, MenuList } = require('./src/index');
 const routes = require('./src/routes/exerciseRoute');
 const routes2 = require('./src/routes/routes');
+const gptRouter = require('./src/routes/gpt/gptRoute');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,6 +15,7 @@ app.use(cors())
 app.use(express.json());
 app.use('/', routes);
 app.use('/', routes2);
+app.use('/gpt', gptRouter);
 
 
 const initializeApp = async () => {
