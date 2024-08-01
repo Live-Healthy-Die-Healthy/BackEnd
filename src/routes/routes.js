06cc2029b5d88322000a4e5f6711dfca3a202abc
8mul <164719127+8mul@users.noreply.github.com>
2024-08-01 14:40:48 +0900

    bug fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -60,9 +60,9 @@ router.post('/checkUser', async (req, res) => {
 
     // BMR 계산 함수 (Harris-Benedict 방정식)
     function calculateBMR(weight, height, age, gender) {
-      if (gender === 'male') {
+      if (gender === 'Male') {
         return 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
-    } else if (gender === 'female') {
+    } else if (gender === 'Female') {
         return 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
     } else {
         throw new Error('Invalid gender');
